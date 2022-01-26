33b364811a09b00557428c14554f8b5acb72ea5b
Brandon Hunsaker <brandondwight42@gmail.com>
2022-01-25 22:48:58 -0700

    Problem 11 working

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -227,7 +227,23 @@ console.log(nameGreeting)
 */
 
 //CODE HERE
+function faveColorFinder(color){
+  if(color === 'red'){
+    return 'red is a great color'
+  }
+  else if(color === 'green'){
+    return 'green is a solid favorite color'
+  }
+  else if(color === 'black'){
+    return 'so trendy'
+  }
+  else{
+    return 'you need to evaluate your favorite color choice'
+  }
+}
 
+let colorRating = faveColorFinder('red')
+console.log(colorRating)
 
 
 ////////////////// PROBLEM 12 ////////////////////
