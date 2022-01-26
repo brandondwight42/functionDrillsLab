020d5b189a8d1f1e9a5b7ebeee8848f70e962358
Brandon Hunsaker <brandondwight42@gmail.com>
2022-01-25 23:20:23 -0700

    Problem 15 returns an empty array

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -313,7 +313,21 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 */
 
 //CODE HERE
+function bigOrSmall(arr){
+  let answers = []
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 100){
+      answers.push[`big`]
+    }
+    else{
+      answers.push[`small`]
+    }
+  }
+  return answers
+}
 
+let arrayEvaluator = bigOrSmall(bigOrSmallArray)
+console.log(arrayEvaluator)
 
 ////////////////// PROBLEM 16 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
